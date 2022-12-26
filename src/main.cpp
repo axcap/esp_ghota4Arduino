d128bff585d109c4088efa11dc96b33c59989689
Akhsarbek Gozoev <gakhsar@gmail.com>
2022-12-26 19:59:23 +0100

    Initial code

---
+++ src/main.cpp
@@ -0,0 +1,117 @@
+/*
+  This example downloads sketch update over WiFi network.
+  You can choose between HTTP and HTTPS connection.
+  In HTTPS case, remember to flash the server root CA certificate
+  using WiFi101/WiFiNINA updater tool.
+  It doesn't start the OTA upload sever of the ArduinoOTA library,
+  it only uses the InternalStorage object of the library
+  to store and apply the downloaded binary file.
+
+  To create the bin file for update of a SAMD board (except of M0),
+  use in Arduino IDE command "Export compiled binary".
+  To create a bin file for AVR boards see the instructions in README.MD.
+  To try this example, you should have a web server where you put
+  the binary update.
+  Modify the constants below to match your configuration.
+
+  Created for ArduinoOTA library in December 2020
+  by Nicola Elia
+  based on Juraj Andrassy sample sketch 'OTASketchDownload'
+*/
+
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h> // only for InternalStorage
+#include <ArduinoHttpClient.h>
+#include <ESP8266httpUpdate.h>
+
+// Insert your WiFi secrets here:
+#include "secrets.h"
+
+#ifndef SECRETS_H_
+#define SECRET_SSID "secret_ssid" // Your network SSID (name)    
+#define SECRET_PASS "secret_password" // Your network password
+#define FIRMWARE_URL "secret_url/update-v%d.bin"
+#endif
+
+const short VERSION = 1;
+WiFiClient client;
+
+void update_started() {
+  Serial.println("CALLBACK:  HTTP update process started");
+}
+
+void update_finished() {
+  Serial.println("CALLBACK:  HTTP update process finished");
+}
+
+void update_progress(int cur, int total) {
+  Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+}
+
+void update_error(int err) {
+  Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.print("Sketch version ");
+  Serial.println(VERSION);
+
+  Serial.println("Initialize WiFi");
+  WiFi.mode(WIFI_STA);
+  //Get Current Hostname
+  Serial.printf("Default hostname: %s\n", WiFi.hostname().c_str());
+
+  String newHostname = "ESP8266 OTA";
+  WiFi.hostname(newHostname.c_str());
+
+  //Get Current Hostname
+  Serial.printf("New hostname: %s\n", WiFi.hostname().c_str());
+
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+  
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+
+  // Add optional callback notifiers
+  ESPhttpUpdate.onStart(update_started);
+  ESPhttpUpdate.onEnd(update_finished);
+  ESPhttpUpdate.onProgress(update_progress);
+  ESPhttpUpdate.onError(update_error);
+}
+
+void loop() {
+  // check for updates
+
+  String PATH = String(FIRMWARE_URL);  // Set your correct hostname
+
+  PATH.replace("%d", String(VERSION + 1));
+  Serial.print("Fetching from: ");
+  Serial.println(PATH);
+
+  auto ret = ESPhttpUpdate.update(client, PATH);
+
+  switch (ret) {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("HTTP_UPDATE_NO_UPDATES");
+      break;
+    case HTTP_UPDATE_OK:
+      Serial.println("HTTP_UPDATE_OK");
+      break;
+  }
+
+  delay(60000);
+}
